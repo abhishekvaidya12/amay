3834494abf389f9d32b20392795c2d3f886bf0bf
abhishekvaidya12 <105770240+abhishekvaidya12@users.noreply.github.com>
2022-05-18 11:14:15 +0530

    Add files via upload

---
+++ 509_Ashutosh.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 256
+int main()
+{
+	int n, ctr = 0;
+	char ch;
+	FILE *fptr1, *fptr2;
+	char fname[MAX];
+	char str[MAX], temp[]="temp.txt";
+	printf("\n\n Delete a specific line from a file :\n");
+	printf("\n\n Delete a specific line from a file :\n");
+	printf("Input the file name to be opened:");
+	scanf("%s",fname);
+	if(! fptr1)
+	{
+		printf("File not found or unable to open the input file");
+		return 0;
+	}
+}
